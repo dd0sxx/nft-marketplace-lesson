166e59dea505d5900e620c4ec5daa0e648ec9ce8
Alex Sumner <alex_sumner@mentor.com>
2021-10-16 10:49:55 +0100

    tidied up funds withdrawal rest

--- test/TigerNFT.js
+++ test/TigerNFT.js
@@ -73,16 +73,20 @@ describe("TigerNFT contract", function () {
     })
 
     it("sellers can withdraw funds", async function () {
+        //artist sells to bob who then sells to alice
         await tiger.connect(artist).putUpForSale(13, ethers.utils.parseEther("1"))
         await tiger.connect(bob).buyTiger(13, {value:ethers.utils.parseEther("1")})
         await tiger.connect(bob).putUpForSale(13, ethers.utils.parseEther("15"))
         await tiger.connect(alice).buyTiger(13, {value:ethers.utils.parseEther("15")})
+
+        //artist withdraws their funds from the sale
         let initialBalance = await ethers.provider.getBalance(artist.address)
         await tiger.connect(artist).withdrawFunds()
         let finalBalance = await ethers.provider.getBalance(artist.address)
         let difference = finalBalance.sub(initialBalance)
         expect(difference).to.be.closeTo(ethers.utils.parseEther("1"), ethers.utils.parseEther("0.04"))
-        await tiger.connect(artist).withdrawFunds()
+
+        //bob withdraws his funds from the sale
         initialBalance = await ethers.provider.getBalance(bob.address)
         await tiger.connect(bob).withdrawFunds()
         finalBalance = await ethers.provider.getBalance(bob.address)
