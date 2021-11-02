08e906bab49bafedc18f7534f3c4530297ec90b6
dd0sxx <contacttheotelonis@gmail.com>
2021-11-02 14:22:12 -0400

    added imgs

--- dapp/src/components/TokenCard.js
+++ dapp/src/components/TokenCard.js
@@ -20,7 +20,7 @@ function TokenCard ({id, provider, address, contract}) {
             <div className='title'>title</div>
             <div>id={id}</div>
             {/* replace the next line background image with token id when all the images go online */}
-            <div className='image' style={{backgroundImage: `url("https://ipfs.io/ipfs/QmVBm9qzvZSPZUF3bYq8QFCMxBdukrfPYi1cGFcYL6wSAY/1.png")`}}></div>
+            <div className='image' style={{backgroundImage: `url("https://ipfs.io/ipfs/QmZWUmdscMCmvvZfNp3BwTvLim26hsDT8BqAYxjFHWjgQ2/${id}.png")`}}></div>
             <div className='price'>{isForSale ? "price: " + ethers.utils.formatEther(price) : "not for sale"}</div>
         </div>
     )
