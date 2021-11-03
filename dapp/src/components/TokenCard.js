e2fb212911c917379eef84dbed0d11940946fdb7
dd0sxx <contacttheotelonis@gmail.com>
2021-11-03 10:18:11 -0400

    token card updates

--- dapp/src/components/TokenCard.js
+++ dapp/src/components/TokenCard.js
@@ -23,8 +23,9 @@ function TokenCard ({id, provider, address, contract}) {
             <div className='image' style={{backgroundImage: `url("https://ipfs.io/ipfs/QmZWUmdscMCmvvZfNp3BwTvLim26hsDT8BqAYxjFHWjgQ2/${id}.png")`}} />
                 {isForSale ?
                     <div className='flex-centered'>
-                        <div style={{backgroundImage: `url("${ethSVG}")`, backgroundRepeat: 'no', backgroundSize: 'contain', width: 20, height: 20}}/>
+                        <div style={{paddingRight: 5}}>price:</div>
                         <div className='price'>{ethers.utils.formatEther(price)}</div>
+                        <div style={{backgroundImage: `url("${ethSVG}")`, backgroundRepeat: 'no', backgroundSize: 'contain', width: 20, height: 20}}/>
                     </div>
                     :
                     <div className='price'>"not for sale"</div>
