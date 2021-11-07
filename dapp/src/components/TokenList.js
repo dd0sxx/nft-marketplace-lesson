b809510ba7b79349168a79f14a503a162ed64889
Alex Sumner <alex_sumner@mentor.com>
2021-11-07 12:44:32 +0000

    removed debug console log statements from TokenList

--- dapp/src/components/TokenList.js
+++ dapp/src/components/TokenList.js
@@ -13,9 +13,7 @@ function TokenList ({provider, address, contract, page, tokensPerPage, totalSupp
             [isForSale, price] = await contract.isForSale(id)
             let token = {id: id, isForSale: isForSale, price: price}            
             setList(a => {
-                console.log(`before a[0]?.id ${a[0]?.id} a[1]?.id ${a[1]?.id} a[2]?.id ${a[2]?.id}`)
                 a.push(token)
-                console.log(`after  a[0]?.id ${a[0]?.id} a[1]?.id ${a[1]?.id} a[2]?.id ${a[2]?.id}`)
                 return a
             })
             // setList(list.push(token))
